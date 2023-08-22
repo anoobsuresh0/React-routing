94f9543541446ac43790dc69f9fc4d5b53c6b981
Anoob Suresh <anoob.suresh@perleybrook.com>
2023-08-23 03:42:41 +0530

    dsad

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import About from './Container/About'
 import Profile from './Container/Profile'
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 
 function App() {
   const [state, setState] = useState('')
-  let component
-  if (state === 'about') {
-    component = <About></About>
-  }
-  if (state === 'profile') {
-    component = <Profile></Profile>
-  }
 
   return (
-    <div>
-      <button onClick={() => setState('about')} >About</button>
-      <button onClick={() => setState('profile')} >My Profile</button>
-      {component}
+    <div className="App">
+      <h1>Hello</h1> 
+      <Link to="about">About Page</Link>
+      <Link to="profile"> Profile Page</Link>
+      <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+       
+      </Routes>
     </div>
+
   )
 }
 
